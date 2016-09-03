b0e75c45e837d55a903de29e54c8b2e44b1f98e6
421p <andrew.bakal@gmail.com>
2016-09-03 23:04:24 +0300

    hello visual studio pls dont do this again

---
+++ adoShopBackend/backend/Entity/OrderRelatedGoods.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace backend.Entity
+{
+    [Table("good_count_pair")]
+    public class OrderRelatedGoods
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("count")]
+        public int Count { get; set; }
+
+        public virtual Good Good { get; set; }
+
+        public virtual Order Order { get; set; }
+    }
+}
