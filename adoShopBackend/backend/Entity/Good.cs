c2c1a593f31b7ba8ce94ee3f2ea0ccedc400f0d4
Alina Shebanova <shebanova.alina@gmail.com>
2016-09-03 15:54:31 +0300

    -shop_entities(not final)

---
+++ adoShopBackend/backend/Entity/Good.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend.Entity
+{
+    [Table("goods")]
+    public class Good
+    {
+        public Good()
+        {
+            GoodsInOrders = new HashSet<GoodsInOrders>();
+        }
+
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("name")]
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Column("supplier_id")]
+        public int? SupplierId { get; set; }
+
+        [Column("category_id")]
+        public int? CategoryId { get; set; }
+
+        [Column("price", TypeName = "money")]
+        public decimal Price { get; set; }
+
+        [Column("amount", TypeName = "numeric")]
+        public decimal Amount { get; set; }
+
+        public virtual Category Category { get; set; }
+
+        public virtual Supplier Supplier { get; set; }
+
+        public virtual ICollection<GoodsInOrders> GoodsInOrders { get; set; }
+    }
+}
---
+++ adoShopBackend/backend/Entity/Order.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend.Entity
+{
+    [Table("orders")]
+    public class Order
+    {
+        public Order()
+        {
+            GoodsInOrders = new HashSet<GoodsInOrders>();
+        }
+
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("date")]
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Column("user_id")]
+        [Required]
+        public int UserId { get; set; }
+
+        //calculate on db level?
+        [Column("sum", TypeName = "money")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public int Sum { get; private set; }
+
+        public virtual ICollection<GoodsInOrders> GoodsInOrders { get; set; }
+    }
+}
