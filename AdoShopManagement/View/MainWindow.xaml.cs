a88c00d99e858e155b0782c839c68e25e1ede171
Alina Shebanova <shebanova.alina@gmail.com>
2016-09-10 15:46:50 +0300

    create management app

---
+++ AdoShopManagement/View/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using GalaSoft.MvvmLight.Messaging;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using System.Linq;
+using System.Windows;
+
+namespace AdoShopManagement.View
+{
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Messenger.Default.Register<NotificationMessage<string>>(this, NotificationMessageReceived);
+        }
+
+        private async void NotificationMessageReceived(NotificationMessage<string> msg)
+        {
+            switch (msg.Notification)
+            {
+                case "ShowMessageBox":
+                    await this.ShowMessageAsync("Info box", msg.Content);
+                    break;
+
+                case "ShowStartWindow":
+                    StartWindow startWindow;
+                    if (Application.Current.Windows.OfType<StartWindow>().Any())
+                        startWindow = Application.Current.Windows.OfType<StartWindow>().First();
+                    else
+                        startWindow = new StartWindow();
+                    Close();
+                    startWindow.Show();
+                    break;
+            }
+        }
+    }
+}
